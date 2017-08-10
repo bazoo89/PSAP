43497657eb488114fa2c7a82bfd4c7b335ac7a21
QuaresimaAn <QuaresimaAn@vt-quaresimaan.valueteam.com>
2017-08-10 16:54:22 +0200

    rinominato fxml custom button in shortcutArea e modificato imageView del cestino in JFXButton

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -456,12 +456,11 @@ public class MainController implements Initializable {
 		penImageView.setOnMouseClicked((MouseEvent event) -> {
 			penAlreadyClicked = true;
 			penImageView.setEffect(ds);
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/CustomButton2.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/ShortcutArea.fxml"));
 			Parent root = null;
 			try {
 				root = loader.load();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			ButtonAreaController baController = loader.getController();
@@ -535,9 +534,9 @@ public class MainController implements Initializable {
 		});
 		ImageView imageView=new ImageView();
 		imageView.setImage(new Image(Main.class.getResourceAsStream("recycleBin.png")));
-		imageView.setFitWidth(32);
-		imageView.setFitHeight(32);
-		imageView.setOnMouseClicked(click->{
+		JFXButton imageBtn=new JFXButton(null,imageView);
+		imageBtn.setPrefSize(33, 33);
+		imageBtn.setOnMouseClicked(click->{
 			baController.removeCustomBtn(customButton.getText());
 		});
 		if(baController.buttonAreaVBox.getChildren().size()==2){
@@ -550,7 +549,7 @@ public class MainController implements Initializable {
 		if(hbox.getChildren().size()<3){
 			HBox customButtonHBox = new HBox();
 			HBox.setMargin(customButtonHBox, new Insets(0, 0, 0, 5.5));
-			customButtonHBox.getChildren().addAll(imageView,customButton);
+			customButtonHBox.getChildren().addAll(imageBtn,customButton);
 			hbox.getChildren().add(customButtonHBox);
 			ToolsForManageFile.getInstance().saveCustomButtonPreferences(TempSavedInformation.getInstance().getPreferencesFile(), customButton.getText());
 		}
